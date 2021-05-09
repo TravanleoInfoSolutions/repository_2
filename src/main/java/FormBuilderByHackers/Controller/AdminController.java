618d67a8ef7fb7c237bc6ff64567a03bb45b7488
Sobha <sobha.edison@travanleo.com>
2021-05-09 20:02:48 +0530

    changes

--- src/main/java/FormBuilderByHackers/Controller/AdminController.java
+++ src/main/java/FormBuilderByHackers/Controller/AdminController.java
@@ -16,7 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
